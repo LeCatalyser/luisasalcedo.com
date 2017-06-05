a60ef5f318af9160623b0dacf5d8188a23e9c9ce
Luisa Salcedo <profe.salcedo@gmail.com>
2017-06-05 17:16:12 -0500

    removed flex box, added click for about and contact

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const currentPage = {
-  currentPage: "homePage"
-};
-$(".link a").on("click", function(event) {
-  const page = $(event.currentTarget).attr("page");
-  console.log(page);
-  $("section").css("display", "none");
-  $(page).css("display", "block");
-  console.log("is it working?");
+$(function() {
+  const currentPage = {
+    currentPage: "homePage"
+  };
+  $(".link a").on("click", function(event) {
+    // const page = $(event.currentTarget).attr("page");
+    // console.log(page);
+    // $("section").css("display", "none");
+    // $(page).css("display", "block");
+    // console.log("is it working?");
+  });
+});
+
+$(".link a").click(function(event) {
+  event.preventDefault();
+  var hash = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    800,
+    function() {}
+  );
 });
